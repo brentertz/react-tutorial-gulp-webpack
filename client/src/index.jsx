c5eda373bb0031430cb195a84e1f04195c676638
Brent Ertz <brent.ertz@gmail.com>
2015-01-25 09:53:21 -0700

    Update deprecated React render syntax

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -5,7 +5,7 @@ var React = window.React = require('react');
 var CommentBox = require('./app/components/comment-box');
 
 $(function() {
-  React.renderComponent(
+  React.render(
     <CommentBox url="/api/comments" pollInterval={3000} />,
     document.getElementById('content')
   );
